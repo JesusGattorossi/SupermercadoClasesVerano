dc1984c9b9f3645bfb48edc3cfab1fea7efaf64a
JesusGattorossi <93558045+JesusGattorossi@users.noreply.github.com>
2022-01-11 17:02:45 -0300

    prinmerCambio

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 console.log("Supermercado Mardel");
-console.log(" azucar, 50");
-console.log(" yerba, 35");
-console.log("azucar, 68");
-console.log(" mermelada, 70");
-console.log(" té, 60");
+console.log(" azucar, 50 unidades");
+console.log(" yerba, 35 unidades");
+console.log("azucar, 68 unidades");
+console.log(" mermelada, 70 unidades");
+console.log(" té, 60 unidades");
+console.log("primer cambio");
